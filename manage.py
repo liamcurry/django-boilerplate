2439d076f9cc1ef4fcfd9cfad91e1b7a43a913b4
Liam Curry <liam@curry.name>
2013-08-03 02:24:49 -0400

    removed old settings; added prod example

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.append(os.path.dirname(os.path.abspath(__file__)))
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
     from django.core.management import execute_from_command_line
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
     execute_from_command_line(sys.argv)
